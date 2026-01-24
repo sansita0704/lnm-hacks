abfbf045c3c41d593010488870c8e95375124998
Sansita Jain <sansita7406@gmail.com>
2026-01-25 03:25:54 +0530

    add chainId

--- AI-Interview/constants/monad.ts
+++ AI-Interview/constants/monad.ts
@@ -1,5 +1,5 @@
 export const MONAD_TESTNET = {
-  chainId: "0x????",        // hex chain id
+  chainId: "10143",        // hex chain id
   chainName: "Monad Testnet",
   nativeCurrency: {
     name: "Monad",
